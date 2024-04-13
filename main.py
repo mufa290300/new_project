6dac9919d81852b7e3433f59029d13ecaf388f84
mufa290300 <166895928+mufa290300@users.noreply.github.com>
2024-04-13 14:29:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ number_2 : float = 30.5
 boolean_var = True
 String_var = 'My String'
 
-print(number + number_2)
-print(number + boolean_var)
-print(number + String_var)
+my_list = [1,2,3,4,5,'String',[1,2,3],100]
+my_map = {
+    'Key_1': 1,
+    'Key_2': 2
+}
+
+if __name__ == '__main__':
+     
